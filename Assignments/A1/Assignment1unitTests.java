c6d70e5954f7c85d825513e02e979de56226bc18
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-09-10 12:03:49 -0400

    Initial commit

---
+++ Assignments/A1/Assignment1unitTests.java
@@ -0,0 +1,65 @@
+/*Your program will calculate the length of a longest contiguous subsequence of equal
+values in a given sequence of integer values. 
+For example if the input sequence is 1,0,0,0,1,1 your program will return 3. 
+Another example: 2,5,5,1,11,11,11,3,5,5,5,5,4,7. Now your
+program should return 4, because there are 4 consecutive values (namely four 5’s) and
+there is no longer subsequence of equal values.*/
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class Assignment1Tests {
+    
+    /**if the input sequence is 1,0,0,0,1,1 your program will return 3. */
+    @Test public void sequence1() {
+        int[] arr = { 1, 0, 0, 0, 1, 1 };
+        final long ANSWER = 3; 
+
+        
+        assertEquals(
+            ANSWER,
+            Assignment1.MaxContinuousSubsequence(arr)
+        );
+    }
+
+    
+    /** 2,5,5,1,11,11,11,3,5,5,5,5,4,7. Now your program should return 4, because there are 4 consecutive values (namely four 5’s) and there is no longer subsequence of equal values.*/
+    @Test public void sequence2(){
+        int[] arr = {2,5,5,1,11,11,11,3,5,5,5,5,4,7};
+        final int ANSWER = 4;
+
+        assertEquals(
+            ANSWER,
+            Assignment1.MaxContinuousSubsequence(arr) 
+        );
+    }
+
+    @Test
+    public void BaseCases(){
+        assertEquals(
+            0,
+            Assignment1.MaxContinuousSubsequence(new int[0])
+        );
+
+        assertEquals(
+            1,
+            Assignment1.MaxContinuousSubsequence(new int[]{0})
+        );
+
+        assertEquals(
+            1,
+            Assignment1.MaxContinuousSubsequence(new int[1000])
+        );
+
+    }
+
+    @Test public void CUSTOM(){
+       
+        
+        assertEquals(1,Assignment1.MaxContinuousSubsequence(new int[100]));
+        
+        
+    }
+
+
+}
