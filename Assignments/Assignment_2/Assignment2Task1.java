e9083ed28d2f94679cabd9096060b44c70d94286
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-09-19 23:24:41 -0400

    added inputs to both

--- Assignments/Assignment_2/Assignment2Task1.java
+++ Assignments/Assignment_2/Assignment2Task1.java
@@ -8,10 +8,13 @@
 
 public class Assignment2Task1 {
 
-    //ADD input
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
 
+        int[] user_sequence = Prompt_Sequence(input);
 
-
+        System.out.print("\nMax Increasing Subsequence: " + MaxIncreasingSubsequence(user_sequence));
+    }
     /** a program that computes the length of a longest increasing subsequence
  of a sequence of integers. * */
     public static int MaxIncreasingSubsequence(int[] arr){
@@ -23,13 +26,9 @@ public class Assignment2Task1 {
         //d[i] corresponds with arr[i]
         int[] d = new int[arr.length]; //keeps track of the COUNTS of the max increasing subsequence 
             d[0] = 1; // d[0] will always be initialized to 1 as will any d[i] as that is the basecase
-            
-
 
         int max_count = 1; //intitialization step is  1
 
-
-        
         for (int i = 1; i < arr.length;i++){ //i is always bigger than j
             d[i] = 1;   //intitialized to 1 for the base case of no increasing subsequences 
             for (int j = 0; j < i; j++){ 
@@ -48,9 +47,6 @@ public class Assignment2Task1 {
 
         return max_count;
     }
-
-
-       
         
       /**reads the initial sequence which is entered by the user*/
     public static int[] Prompt_Sequence(java.util.Scanner input){ 
