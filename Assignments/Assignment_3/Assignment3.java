1bc4d1250b011395b7eeb0bd5478a639120b319e
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-09-26 21:25:48 -0400

    books merge sort fixed
    Books merge sort now passes tests and is the one being used to complete assignment

--- Assignments/Assignment_3/Assignment3.java
+++ Assignments/Assignment_3/Assignment3.java
@@ -6,98 +6,48 @@ public class Assignment3 {
 
     //TODO: pairs method
     public static int UP_Pairs(int[] arr){
-        if (arr.length <= 1) return 0;
-        if (arr.length == 2){
-            if (arr[0] < arr[1]) return 1;
-
-            return 0;
-        }
-
-        final int MID = (arr.length / 2)  ;
-
-        int[] L = new int[MID]; //could be n1
-        int[] R = new int[arr.length - MID]; //could be n2
-
-        //array copying step using System.arraycopy instead of for loop manual copy
-        System.arraycopy(arr, 0, L, 0, MID);
-        System.arraycopy(arr, MID, R, 0, arr.length - MID);
-
-        return merge(arr,L,R) + UP_Pairs(L) + UP_Pairs(R);
-       
-
+        return merge(arr, 0, arr.length / 2, arr.length) + 1;
         
-    }
-
-    //working merge sort for testing
-    public static void mergeSort(int[] arr) {
-        if (arr.length <= 1) 
-            return;
-        
-        final int MID = arr.length / 2;
-
-        int[] L = new int[MID]; //could be n1
-        int[] R = new int[arr.length - MID]; //could be n2
-
-        //array copying step using System.arraycopy instead of for loop manual copy
-        System.arraycopy(arr, 0, L, 0, MID);
-        System.arraycopy(arr, MID, R, 0, arr.length - MID);
-
-
-        //recursively sorts the halves; does the above process until the basecase of 1 and then sorts them and merges them into the main array
-        mergeSort(L);
-        mergeSort(R);
-
-        // Merge the sorted halves
-        merge(arr, L, R);
-    }
-
-
-    //working merge method for testing
-    public static int merge(int[] arr, int[] L, int[] R) {
- 
-        /** i is index of left , j is index of right, k is absolute index of main array just like the mergesort in lect4*/
-        int i = 0, j = 0, k = 0;
-   
-        //k always increases while i or j will eventually stop ;same as the textbooks way of sorting it 
-        while (i < L.length && j < R.length) {
-            if (L[i] <= R[j]) 
-                arr[k++] = L[i++];
-             else 
-                arr[k++] = R[j++];
-        }
-
-        
-        // Copies whatever is left into the main array; whatever space is leftabsolute position k is calculated by (array.length-k)
-        if (i < L.length)
-            System.arraycopy(L,i,arr,k,arr.length-k);
-
-        if (j < R.length)
-            System.arraycopy(R,j,arr,k,arr.length-k);
-
-        return i;
     }
 
     
 
-    //the books merge method //TODO: fix the books version
-    public static void merge(int[] arr, int p ,int q , int r){
-        if (arr.length <= 1 || p > r) return;
-        
-        int n1 = q - p + 1;
-        int n2 = r - q;
+    /** modified merge method to return UP pairs
+     * @param arr the main array
+     * @param  p left index
+     * @param  q middle index
+     * @param  r right index
+     * @return UP pair count 
+    */
+    public static int merge(int[] arr, int p ,int q , int r){
+        if (arr == null || arr.length <= 1 || p > r) return 0; //if null or length less than 1 or left index bigger than right index,
+       
+
+        //removed + 1 for left index cus book was using 1 based indexing for some reason
+        int n1 = q - p ; 
+        int n2 = r - q; 
+
+        int up_pairs = 0;
 
         int[] L = new int[n1];
         int[] R = new int[n2];
 
+        //puts values in L and R from main array
         for (int i = 0; i < n1; i++)
             L[i] = arr[p + i];
 
         for (int j = 0; j < n2; j++)
-            R[j] = arr[q + j + 1];
+            R[j] = arr[q + j];
 
+        //sorts L and R 
+        up_pairs += merge(L,0,n1/2,n1) ;
+        up_pairs += merge(R,0,n2/2,n2);
+        
+        //i is left index, j is right index, k is main array index
         int i = 0;
         int j = 0;
         int k = 0;
+
         while (i < n1 && j < n2) { 
             if (L[i] <= R[j]) {
                 arr[k] = L[i];
@@ -110,16 +60,16 @@ public class Assignment3 {
             k = k + 1;
         }
 
+        //since i is also how many in L are smaller than R everything in it is a guaranteed pair
+        up_pairs += i;
 
         
-        
-
-        //copies the remainder
         while (i < n1) {
             arr[k] = L[i];
             i++;
             k++;
         }
+       
 
         while (j < n2) {
             arr[k] = R[j];
@@ -127,6 +77,7 @@ public class Assignment3 {
             k++;
         }
 
+        return up_pairs;
     }
 
 
--- Assignments/Assignment_3/Assignment3UnitTests.java
+++ Assignments/Assignment_3/Assignment3UnitTests.java
@@ -5,30 +5,32 @@ import junit.framework.Assert;
 public class Assignment3UnitTests extends Assignment3{
     @Test 
     public void Merge_Sort(){
-        int[] arr = {3, 2, 1, 6, 5 ,4 };
+        int[] arr = {3,2,1, 6, 5 ,4 };
         int[] expected = { 1, 2, 3, 4, 5 , 6 };
            
-        merge(arr,0,arr.length/2,arr.length-1);
+        merge(arr,0,arr.length/2,arr.length);
         assertArrayEquals(expected, arr);
-
 
         int[] arr2 = {6,5,4,68,3,2,1,0};
         int[] expected2 = {0, 1, 2, 3, 4, 5 , 6 ,68};
            
-        mergeSort(arr2);
+        merge(arr2,0,arr2.length/2,arr2.length);
         assertArrayEquals(expected2, arr2);
-
-        
-       
     }
 
     @Test 
     /**  for example input sequence 7,3,8,1,5, we have the following UP-pairs: (7, 8), (3,8),(3,5),(1,5). */
     public void UP_Pairs(){
         int[] arr = {7,3,8,1,5};
-        int expected = 4;
+            Assert.assertEquals(4, merge(arr, 0, arr.length/2, arr.length));
+
+        int[] arr2 = {1,2};
+            Assert.assertEquals(1, UP_Pairs(arr2));
 
 
-        Assert.assertEquals(expected, UP_Pairs(arr));
+        int[] arr3 = {3,2,1,0};
+            Assert.assertEquals(0, UP_Pairs(arr3));
+
+
     }
 }
