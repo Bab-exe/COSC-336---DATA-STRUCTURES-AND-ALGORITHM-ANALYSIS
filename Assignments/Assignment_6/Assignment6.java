3fbb7a0ebb4da72f54a8630d146ed4dbb430b114
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-10-29 12:56:53 -0400

    Update Assignment6.java

--- Assignments/Assignment_6/Assignment6.java
+++ Assignments/Assignment_6/Assignment6.java
@@ -1,137 +1,132 @@
 
 /** Since only 2 files are submitted (pdf and .java) all classes are stuffed into this file */
-
-
-
-
 public class Assignment6 {
+
+    
+
     public static void main(String[] args) {
-        BinarySearchTree node = new BinarySearchTree();
+        Node root = new Node(50);
 
 
-        // Creating the following BST
+        // Creates tree
         //      50
         //     /  \
         //    30   70
         //   / \   / \
         //  20 40 60 80
 
-        node.insert(50);
-        node.insert(30);
-        node.insert(30);
-        node.insert(20);
-        node.insert(40);
-        node.insert(70);
-        node.insert(60);
-        node.insert(80);
-        
+        insert(root,50);
+        insert(root,30);
+        insert(root,30);
+        insert(root,20);
+        insert(root,40);
+        insert(root,70);
+        insert(root,60);
+        insert(root,80);
 
-
-        
         // Print inorder traversal of the BST
-        node.inorder();
+        inorder(root);
         
+        // Searching for keys in the BST
+        System.out.println(search(root, 19) != null
+                               ? "Found"
+                               : "Not Found");
+        System.out.println(search(root, 80) != null
+                               ? "Found"
+                               : "Not Found");
     }
+    
+    /** function to search a key in a BST. */
+    static Node search(Node root, int key)
+    {
+        // Base Cases: root is null or key is present at
+        // root
+        if (root == null || root.key == key)
+            return root;
+
+        // Key is greater than root's key
+        if (root.key < key)
+            return search(root.right, key);
+
+        // Key is smaller than root's key
+        return search(root.left, key);
+    }
+
+   
+    /** rotates the root t to the left, so that the right child of t (if there is one; otherwise the rotation does not do anything) becomes the parent of t, and symmetrically rightRotate  (Node t) */
+ static Node leftRotate(Node root,final Node t) {
+    Node r = t.right;
+    t.right = r.left;
+    r.left = t;
+    return r;
 }
-class BinarySearchTree {
-    private Node root;
-    private int height;
-    public BinarySearchTree() {
-        this.root = null;
-        this.height = 0;
-    }
 
-    public int getRoot() {
-        return this.root.key;
-    }
-    public Node insert(int key) {
-        if (this.root == null) return this.root = new Node(key);
-        else return this.root = insert(this.root, key);    
-    }
-
-    private Node insert(Node root, int key) {        
-        if (root == null) root = new Node(key); //basecase
-        
-        if (key < root.key) root.left = insert(root.left, key);
-        if (key > root.key) root.right = insert(root.right, key);
-        return root;   
-    }
+static Node insert(Node root, int key) {        
+    if (root == null) root = new Node(key); //basecase
     
-    public Node search(int key) {
-        return search(this.root, key);
-    }
+    if (key < root.key) root.left = insert(root.left, key);
+    if (key > root.key) root.right = insert(root.right, key);
+    return root;   
+}
 
-    /** function to search a key in a BST
-     * @param root
-     * @param key
-    */
-    private Node search (Node root, int key) {
-        if (root == null) return null;
+/** function to search a key in a BST
+ * @param root
+ * @param key
+*/
+static void inorder(Node root){
+    if(root == null) return;
 
-        if (root.key == key) return root;
-        if (key < root.key) return search(root.left, key);
-        return search(root.right, key);
-    }
+    inorder(root.left);
+    System.out.print(root.key + " ");
+    inorder(root.right);
+}
 
-    private void inorder(Node root){
-        if(root == null) return;
 
-        inorder(root.left);
-        System.out.print(root.key + " ");
-        inorder(root.right);
-    }
 
-    public void inorder() {
-        inorder(this.root);
-    }
-
-    public Node delete(Node root, int x) {
-        
-        // Base case
-        if (root == null) return root;
+static Node delete(Node root, int x) {
     
-        // If key to be searched is in a subtree
-        if (root.key > x) {
-            root.left = delete(root.left, x);
-        } else if (root.key < x) {
-            root.right = delete(root.right, x);
-        } else {
-            // If root matches with the given key
+    // Base case
+    if (root == null) return root;
 
-            // Cases when root has 0 children or
-            // only right child
-            if (root.left == null) return root.right;
+    // If key to be searched is in a subtree
+    if (root.key > x) {
+        root.left = delete(root.left, x);
+    } else if (root.key < x) {
+        root.right = delete(root.right, x);
+    } else {
+        // If root matches with the given key
+
+        // Cases when root has 0 children or
+        // only right child
+        if (root.left == null) return root.right;
+        
+
+        // When root has only left child
+        if (root.right == null) return root.left;
+        
+
+        // When both children are present
+        Node parent;
+        {
+            Node current = root.right;
+            while (current != null && current.left != null) 
+                current = current.left;
             
-
-            // When root has only left child
-            if (root.right == null) return root.left;
-            
-
-            // When both children are present
-            Node parent;
-            {
-                Node current = root.right;
-                while (current != null && current.left != null) 
-                    current = current.left;
-                
-                parent = current;
-            }
-            
-
-            root.key = parent.key;
-            root.right = delete(root.right, parent.key);
+            parent = current;
         }
-        return root;
-    }
-    public Node delete(int key) {
         
-        return this.root = delete(this.root,key);
-        
+
+        root.key = parent.key;
+        root.right = delete(root.right, parent.key);
     }
-    
+    return root;
+}
+
+}
+
+   
 
     
-}
 class Node {
     int key;
     Node left, right;
