7f543757a3cd9c208be8fe11ee86dc2e95f2cbd6
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-11-11 20:11:34 -0500

    File Reading Test Passing

--- Assignments/Assignment_7/Assignment7.java
+++ Assignments/Assignment_7/Assignment7.java
@@ -1,12 +1,59 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 class Assignment7{
 
     public static void main(String[] args) {
-        Adj_List_Graph a = new Adj_List_Graph(5);
-        a.addEdge(0, 1);
-        a.addEdge(2, 3);
-        a.addEdge(4, 5);
-        a.addEdge(6, 7);
+        
+        Adj_List_Graph input7_1 = inputFile("input-7-1.txt");
+        Adj_List_Graph input7_2 = inputFile("input-7-2.txt");
 
-        a.printGraph();
+        System.err.print("input-7-1.txt: ");
+            input7_1.printGraph();
+           
+        System.err.print("\ninput-7-2.txt: ");
+            input7_2.printGraph();
     }
+
+    /** reads the file and converts it to an  adjacency list */
+    public static Adj_List_Graph inputFile(String filename) {
+        try (Scanner console = new Scanner(new FileReader(filename))) {
+            //node count
+            int N = console.nextInt();
+                   
+            final Adj_List_Graph GRAPH = new Adj_List_Graph(N);
+
+            //add edges
+            for (int u = 0 ; u < N;u++)
+                for (int v = 0; v < N; v++)
+                    GRAPH.adj.get(u).add(console.nextInt());
+                   
+            return GRAPH;
+        }catch(FileNotFoundException e) {
+            System.err.println("File not found: '" + filename + "'");
+        }
+
+        return null;
+    } 
+
+
+    public static Adj_List_Graph getAdjacencyList(final Adj_List_Graph GRAPH) {
+        /** The n nodes are labeled 0,1,...,n −1. If the i×n+j-th bit in the sequence is
+ 1, then there is an edge from node i to node j, and if the i × n+j-th bit in the sequence
+ is 0, then there is no edge from node i to node j. In other words, if the n2 bits are indexed
+ with indices 0,1,...,n2 − 1, from the bit with index k, you compute i = k/n and j = k
+ (mod n) and if bit with index k is 1, then there exists an edge (i,j), and if it is 0, then
+ there is no edge (i,j). For example, the graph G above has n = 4 and the edges are given
+ by the following n2 = 16 bits: 0 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0.  */
+        final int N2 = GRAPH.n * GRAPH.n;
+
+
+        //for (int i = 0; i < )
+        return null;
+    }
+
+    
+    
+    //TODO: Copy and Paste Adj_List_Graph class when Submitting
 }
--- Assignments/Assignment_7/Assignment7Tests.java
+++ Assignments/Assignment_7/Assignment7Tests.java
@@ -1,10 +1,49 @@
 
+import java.util.ArrayList;
 import org.junit.Test;
 
-public class Assignment7Tests extends Assignment7 {
+
+public class Assignment7Tests extends Assignment7  {
     @Test
-    public void test1() {
-     
+    public void File_Reading() {
+        Adj_List_Graph input7_1 = inputFile("input-7-1.txt");
+        Adj_List_Graph input7_2 = inputFile("input-7-2.txt");
+       
+        assert input7_1 != null; //file exists
+        assert input7_1.n == 3; //3 nodes
+        
+        //all sizes should be the same
+        for (ArrayList<Integer> list1 : input7_1.adj) {
+            assert list1 != null;
+            assert list1.size() == 3;
+        }
+
+        assert input7_2 != null;
+        assert input7_2.n == 5;
+        for (ArrayList<Integer> list2 : input7_2.adj) {
+            assert list2 != null;
+            assert list2.size() == 5;
+        }
+
+        System.err.println("input-7-1.txt: ");
+            print_Matrix(input7_1);
+
+        System.err.println("\ninput-7-2.txt: ");
+            print_Matrix(input7_2);
     }
     
+
+    public void print_Matrix(final Adj_List_Graph GRAPH) {
+        final int N = GRAPH.n;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < GRAPH.adj.get(i).size(); j++) 
+                System.out.print(GRAPH.adj.get(i).get(j) + " ");
+            
+            System.out.println();
+        
+        }
+
+    
+            
+    }
 }
