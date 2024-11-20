0feea5501422af26249a7ca21afa8a63c47f44e0
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-11-20 18:12:40 -0500

    All Tests Passing
    Assignment8 java Completed?

--- Assignments/Assignment_8/Adj_List_Graph.java
+++ Assignments/Assignment_8/Adj_List_Graph.java
@@ -1,90 +1,70 @@
-import java.util.*;
-import java.io.*;
-
-
+import java.util.ArrayList;
 
   
   public class Adj_List_Graph{
     int n;  // no of nodes
     ArrayList<ArrayList<Integer> > adj; 
-    final boolean DIRECTED;
+
+    /** Exists for display purposes; <br> assignment 8 uses 1 based indexing <br>
+     * <code>
+     * INDEX_OFFSET = 0 would be 0 based indexing <br>
+     * INDEX_OFFSET = 1 would be 1 based indexing <br>
+     * etc
+     * </code>
+     */
+    final int INDEX_OFFSET; //added to control indexing when displaying graphs for assignment 8
+   
     
     //constructor taking as the single parameter the number of nodes
-    Adj_List_Graph(int no_nodes, final boolean DIRECTED_GRAPH) {
-      this.DIRECTED = DIRECTED_GRAPH;
+    Adj_List_Graph(int no_nodes,final int INDEX_OFFSET) {
       n = no_nodes;
       adj =  new ArrayList<ArrayList<Integer>>(n);
       for (int i = 0; i < n; i++)
         adj.add(new ArrayList<Integer>());
-      
+
+      this.INDEX_OFFSET = INDEX_OFFSET; //this was added for displaying assignment 8 
     }
 
-    /** graph is undirected by default for assignment 8 */
     public Adj_List_Graph(int no_nodes) {
-        this(no_nodes, false);
+        this(no_nodes, 1); //INDEX_OFFSET is set to 1 by default for assignment 8
     }
 
-    
-
-    
-
-    
-    
     // A utility function to add an edge in an
     // undirected graph; for directed graph remove the second line
-    public void addEdge(int u, int v)
+    //adjusted for assignment 8
+    public void addEdge(final int U, final int V)
     {
+      int u = U - (this.INDEX_OFFSET); //adjusted u
+      int v = V - (this.INDEX_OFFSET); //adjusted v
       adj.get(u).add(v);
-      if (this.DIRECTED) return;
-
+      
+      //undirected for assignment 8
        adj.get(v).add(u);  //this line should be un-commented, if graph is undirected
     }
     
     /** A utility function to print the adjacency list representation of graph */
-    //this function was not changed
+    //adjusted for assignment 8
     public void printGraph()
     {
       for (int i = 0; i < n; i++) {
-        System.out.print("\nAdjacency list of vertex" + i);
-        System.out.print("\thead");
-        for (int j = 0; j < adj.get(i).size(); j++) {
-          System.out.print(" -> "+adj.get(i).get(j));
+        System.out.print("\nAdjacency list of vertex" + (i+this.INDEX_OFFSET) + "\thead");
+        for (int j : adj.get(i)) {
+          System.out.print(" -> "+ (j + this.INDEX_OFFSET));
         }
-        System.out.println();
       }
     }
 
-    /** A utility function to print the adjacency <b>list</b> representation of graph 
-     * @param INDEX_OFFSET the starting vertex
-    */
-    //this function was added 
-    public void printList(final int INDEX_OFFSET){
 
-      int i = INDEX_OFFSET; //adjusted index 
-      System.err.printf("\n[%d] Based Indexing:" , INDEX_OFFSET);
-      final String ARROW = (this.DIRECTED) ? " -> " : " <>  ";
-      for (ArrayList <Integer> u : this.adj) {
-          System.out.printf("\n%d: ", i);
-          
-          for (int v = 0; v < u.size(); v++)
-              if (u.get(v) != 0)
-                  System.out.print( ARROW + (u.get(v)+INDEX_OFFSET));   
-          System.out.println();
-          i++;
-      }
-    }
-
-  
-    public void print_Adjacency_Matrix(){
-      System.out.println("\nAdjacency Matrix:");
+    /** A utility function to print the adjacency matrix representation of graph */
+    public void print_AdjacencyMatrix(){
+      System.out.println("\n\nAdjacency Matrix:");
       for (int u = 0;u < n ; u++){
         for (int v = 0 ; v < n ; v++){
           System.out.printf(
               "%d  ", 
               adj.get(u).contains(v) ? 1 : 0
           );
-        }
-        
+        } 
         System.out.println();
       }
     }
