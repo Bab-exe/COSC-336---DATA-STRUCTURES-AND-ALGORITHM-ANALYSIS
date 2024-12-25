e44f9489df919c48a21abb39d2767fee21bdd618
Blessing <96212794+Bab-exe@users.noreply.github.com>
2024-12-25 18:48:25 -0500

    Update Assignment3.java

--- Assignments/Assignment_3/Assignment3.java
+++ Assignments/Assignment_3/Assignment3.java
@@ -36,9 +36,9 @@ public class Assignment3 {
         int[] R = new int[n2];
 
         // Fill left and right arrays
-        for (int i = 0; i < n1; i++) {
+        for (int i = 0; i < n1; i++) 
             L[i] = A[p + i];
-        }
+        
         for (int j = 0; j < n2; j++) 
             R[j] = A[mid + j];
 
